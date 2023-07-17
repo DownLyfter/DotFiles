2be84e63873cd664155dae5379aa79cdf1c2e85d
DownLyfter <thoirvine@gmail.com>
2023-07-17 09:51:44 -0600

    removal

--- Arch/autorun/premid.sh
+++
@@ -1,1 +0,0 @@
-~/.config/autorun/PreMid.AppImage --no-sandbox
--- Arch/scripts/solo.sh
+++
@@ -1,8 +0,0 @@
-#/bin/bash
-PID=$(xprop | grep PID | grep -Eo '[0-9]{1,9}')
-echo $PID
-kill -STOP $PID
-echo "Froze Game"
-sleep 10
-kill -CONT $PID
-echo "Done"
